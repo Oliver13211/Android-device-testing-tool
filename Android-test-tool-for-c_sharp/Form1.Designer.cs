fc16bbe9b239b4a8137dd535f50f30b4d3c0e57b
Oliver <2995306790@qq.com>
2023-10-25 18:55:19 +0800

    v1.1 ui改动

---
+++ Android-test-tool-for-c_sharp/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Android_test_tool_for_c_sharp
+{
+    partial class Form1
+    {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            statusStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(40, 56);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 36);
+            button1.TabIndex = 3;
+            button1.Text = "开始";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(113, 27);
+            numericUpDown1.Maximum = new decimal(new int[] { 21, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(53, 23);
+            numericUpDown1.TabIndex = 1;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 412);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(860, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(56, 17);
+            toolStripStatusLabel1.Text = "准备就绪";
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(860, 25);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = Properties.Resources.关于;
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Text = "关于";
+            toolStripButton1.Click += toolStripButton1_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = Properties.Resources.avgvq_w6rmb_001;
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(23, 22);
+            toolStripButton2.Text = "导出";
+            toolStripButton2.Click += toolStripButton2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(6, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 25);
+            label1.TabIndex = 0;
+            label1.Text = "重启次数：";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 22);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(437, 353);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(411, 28);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(449, 381);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "输出内容";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(numericUpDown1);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Location = new Point(12, 28);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(200, 100);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "重启";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(860, 434);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(toolStrip1);
+            Controls.Add(statusStrip1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "调试工具";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private NumericUpDown numericUpDown1;
+        private Button button1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private Label label1;
+        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private ToolStripButton toolStripButton2;
+    }
+}
---
+++ Android-test-tool-for-c_sharp/Form2.Designer.cs
@@ -0,0 +1,167 @@
+﻿using System.Security.Policy;
+
+namespace Android_test_tool_for_c_sharp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            groupBox2 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            groupBox3 = new GroupBox();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(pictureBox1);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Location = new Point(680, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(280, 523);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "关于本软件";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.工具;
+            pictureBox1.Location = new Point(97, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 95);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(87, 369);
+            label6.Name = "label6";
+            label6.Size = new Size(128, 17);
+            label6.TabIndex = 3;
+            label6.Text = "制作不易，感谢使用！";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(13, 186);
+            label5.Name = "label5";
+            label5.Size = new Size(254, 17);
+            label5.TabIndex = 2;
+            label5.Text = "错误反馈请发邮件至：2995306790@qq.com";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(102, 158);
+            label4.Name = "label4";
+            label4.Size = new Size(78, 17);
+            label4.TabIndex = 1;
+            label4.Text = "作者：Oliver\r\n";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(97, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 17);
+            label3.TabIndex = 0;
+            label3.Text = "安卓设备测试工具";
+            label3.Click += label3_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(webView21);
+            groupBox3.Location = new Point(12, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(662, 523);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "帮助";
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(0, 17);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(662, 506);
+            webView21.Source = new Uri("file:///" + AppDomain.CurrentDomain.BaseDirectory + "help.html", UriKind.Absolute);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(961, 547);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form2";
+            Text = "关于";
+            Load += Form2_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBox2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox3;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
