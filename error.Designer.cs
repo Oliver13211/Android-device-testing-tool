50308973a58554c578476c6275b11bdbb5e25dc2
LISMART-PC\wangy <2995306790@qq.com>
2023-12-15 16:50:21 +0800

    v1.2.1 无用户层面修改 仅添加开源许可证信息

--- error.Designer.cs
+++ error.Designer.cs
@@ -39,9 +39,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.IMG_202311141498_99x99;
-            pictureBox1.Location = new Point(63, 77);
+            pictureBox1.Location = new Point(30, 55);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(104, 107);
+            pictureBox1.Size = new Size(95, 94);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
@@ -49,9 +50,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("MV Boli", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(218, 32);
+            label1.Location = new Point(139, 23);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(85, 26);
+            label1.Size = new Size(60, 18);
             label1.TabIndex = 1;
             label1.Text = "OOPS！";
             // 
@@ -59,17 +61,19 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Microsoft Yi Baiti", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(218, 77);
+            label2.Location = new Point(139, 55);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(395, 56);
+            label2.Size = new Size(282, 38);
             label2.TabIndex = 2;
             label2.Text = "程序发生了错误\r\n原因：您未选择文件的保存位置\r\n";
             // 
             // button1
             // 
-            button1.Location = new Point(368, 177);
+            button1.Location = new Point(234, 125);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(71, 24);
             button1.TabIndex = 3;
             button1.Text = "继续";
             button1.UseVisualStyleBackColor = true;
@@ -77,15 +81,16 @@
             // 
             // Error
             // 
-            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(867, 282);
+            ClientSize = new Size(552, 200);
             Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Error";
